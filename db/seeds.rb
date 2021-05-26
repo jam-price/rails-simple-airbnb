9dcc232043bfcb302df4c6ef1dc547d622165107
James Price <jamesmpdev@gmail.com>
2021-05-26 15:51:31 +0100

    adding bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating flats'
+
+Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  )
+  Flat.create!(
+    name: 'Nice flat',
+    address: 'Brighton',
+    description: 'Beautiful',
+    price_per_night: 150,
+    number_of_guests: 4
+  )
+  Flat.create!(
+    name: 'HOUSE',
+    address: 'LONDON',
+    description: 'Absolutely charming',
+    price_per_night: 120,
+    number_of_guests: 1
+  )
+  Flat.create!(
+    name: 'Cottage',
+    address: 'Brighton',
+    description: 'So nice',
+    price_per_night: 110,
+    number_of_guests: 3
+  )
+  Flat.create!(
+    name: 'Mansion',
+    address: 'Essex',
+    description: 'Huge',
+    price_per_night: 10000,
+    number_of_guests: 25
+  )
